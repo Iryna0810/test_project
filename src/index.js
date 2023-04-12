46478afcf04ddba25a50d1099452f5a64d32a649
Rew <irinavakulenko2000@gmail.com>
2023-04-12 23:40:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,56 @@
 import axios from 'axios';
-import createGalleryCard from '/gallery_list.hbs';
+import createGalleryCard from './gallery_list.hbs';
 
-class MoviesDatabase {
-    #API_KEY = '2bcb7fdd81c3309c5e646690433e3287';  
-    #BASE_URL = 'https://developers.themoviedb.org/3/trending/get-trending';
+   const API_KEY = '2bcb7fdd81c3309c5e646690433e3287';  
+   const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
 
-    query = null;
-    page = 1;
-    count = 20;
+// class MoviesDatabase {
+ 
+//     media_type = 'movie';
+//     time_window = 'week';
 
-    baseSearchParameters = {
-        // per_page: this.count,
-        key: this.#API_KEY,
-        original_title: 'original_title',
-        // orientation: 'horizontal',
-        // safesearch:'true',
-    };
+        
+//     query = null;
+//     page = 1;
+//     count = 20;
 
-    async fetchVideo () {
+//     baseSearchParameters = {
+//         // per_page: this.count,
+//         key: this.#API_KEY,
 
-        const searchParameters = new URLSearchParams({
-            q: this.query,
-            // page: this.page,
-            ...this.baseSearchParameters,
-        });
+//         // orientation: 'horizontal',
+//         // safesearch:'true',
+//     };
+
+
+// };
+
+     async function fetchVideo () {
+
+        // const searchParameters = new URLSearchParams({
+        //     // q: this.query,
+        //     // page: this.page,
+        //     ...this.baseSearchParameters,
+        // });
 
         try {
-            return await axios.get(`${this.#BASE_URL}/?${searchParameters}`);
+            return await axios.get(`${BASE_URL}?api_key=${API_KEY}`);
         }
         
         catch (error) {
             throw new Error(error.message);
         }
     };
-}
 
 const imagesList = document.querySelector('.gallery');
 
-const galleryItems = MoviesDatabase.fetchVideo(data);
+console.log(fetchVideo());
 
-const markup = galleryItems.map((data) => createGalleryCard(data)).join("");
+const galleryItems = fetchVideo();
+
+// console.log({ data });
+
+
+const markup = galleryItems.map(({ data }) => createGalleryCard({ data })).join(" ");
+
+imagesList.insertAdjacentHTML('afterbegin', markup);
