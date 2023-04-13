d7fcaf711308de747a1ddaa6fd0f291d9c9065d9
Rew <irinavakulenko2000@gmail.com>
2023-04-13 23:49:30 +0300

    f

--- src/index.js
+++ src/index.js
@@ -4,35 +4,7 @@ import createGalleryCard from './gallery_list.hbs';
    const API_KEY = '2bcb7fdd81c3309c5e646690433e3287';  
    const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
 
-// class MoviesDatabase {
- 
-//     media_type = 'movie';
-//     time_window = 'week';
-
-        
-//     query = null;
-//     page = 1;
-//     count = 20;
-
-//     baseSearchParameters = {
-//         // per_page: this.count,
-//         key: this.#API_KEY,
-
-//         // orientation: 'horizontal',
-//         // safesearch:'true',
-//     };
-
-
-// };
-
      async function fetchVideo () {
-
-        // const searchParameters = new URLSearchParams({
-        //     // q: this.query,
-        //     // page: this.page,
-        //     ...this.baseSearchParameters,
-        // });
-
         try {
             return await axios.get(`${BASE_URL}?api_key=${API_KEY}`);
         }
@@ -46,11 +18,28 @@ const imagesList = document.querySelector('.gallery');
 
 console.log(fetchVideo());
 
-const galleryItems = fetchVideo();
+fetchVideo()
+    .then(({ data }) =>
+        imagesList.innerHTML = createGalleryCard(data.results))
+    .catch(err => {
+            console.log(err);
+    });
 
-// console.log({ data });
+const wathedFilmAdd = document.querySelectorAll('button');
+const queueFilmAdd = document.querySelector('#Queue');
+let watchedListFilm = [];
 
+wathedFilmAdd.addEventListener('click', getWatchedListFilms);
 
-const markup = galleryItems.map(({ data }) => createGalleryCard({ data })).join(" ");
+function getWatchedListFilms(event) {
+    console.log(event);
+    const savedWatched = localStorage.setItem("watched", JSON.stringify()); 
+    return savedWatched;
+}
 
-imagesList.insertAdjacentHTML('afterbegin', markup);
+// const savedWatched = localStorage.getItem("Watched");
+
+// const parsedWatched = JSON.parse(savedWatched);
+// console.log(parsedWatched); 
+
+// const savedSettings = localStorage.getItem("Watched");
